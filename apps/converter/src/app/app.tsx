9adfc8deeb4de18c9f8fee38816396525ab0dbb6
Noé Gambini <ngambini@macbook-pro-de-noe.home>
2020-07-15 18:00:03 +0200

    Implemented basic functionality

--- apps/converter/src/app/app.tsx
+++ apps/converter/src/app/app.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import { Button } from '@test-oclock/components';
+import { IReduxState } from '../store/root.state';
 
-export const App = ({ dispatch }) => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./app.less file.
-   */
+// todo type props
+export const App = ({ dispatch, symbols, rates }) => {
+  const [toConvert, setToConvert] = useState(0);
+  const [targetCurrency, setTargetCurrency] = useState(null);
+
   return (
     <div>
       <Button
@@ -16,10 +16,34 @@ export const App = ({ dispatch }) => {
           dispatch({ type: 'SYMBOLS_FETCH_REQUESTED' });
           dispatch({ type: 'RATES_FETCH_REQUESTED' });
         }}
-        title="btn from storybook"
+        title="get symbols and rates"
       />
+      <input
+        type="number"
+        value={toConvert}
+        onChange={(e) => setToConvert(parseInt(e.target.value))}
+      />
+      <select
+        value={targetCurrency}
+        onChange={(e) => setTargetCurrency(e.target.value)}
+      >
+        {Object.keys(symbols).map((key) => (
+          <option value={key}>{symbols[key]}</option>
+        ))}
+      </select>
+      {toConvert && targetCurrency && (
+        <div>
+          result : {toConvert * rates.rates[targetCurrency]}{' '}
+          {symbols[targetCurrency]}
+        </div>
+      )}
     </div>
   );
 };
 
-export default connect()(App);
+const mapStateToProps = (state: IReduxState) => ({
+  symbols: state.symbols,
+  rates: state.rates, // todo make this a precise selector
+});
+
+export default connect(mapStateToProps)(App);
